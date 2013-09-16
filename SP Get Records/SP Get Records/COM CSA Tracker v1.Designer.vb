15cce213ac4b0782c96a21db3fd30a55109304d4
John <johntamares@gmail.com>
2013-09-17 00:36:25 +0800

    initial

---
+++ SP Get Records/SP Get Records/COM CSA Tracker v1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class COM_CSA_Tracker_v1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle7 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle8 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle9 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.bDownloadCSATracker = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.My_BgWorker = New System.ComponentModel.BackgroundWorker()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.Panel1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.bDownloadCSATracker)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(757, 41)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(618, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Total Records: 99,999"
+        Me.Label1.Visible = False
+        '
+        'bDownloadCSATracker
+        '
+        Me.bDownloadCSATracker.Location = New System.Drawing.Point(12, 7)
+        Me.bDownloadCSATracker.Name = "bDownloadCSATracker"
+        Me.bDownloadCSATracker.Size = New System.Drawing.Size(164, 23)
+        Me.bDownloadCSATracker.TabIndex = 0
+        Me.bDownloadCSATracker.Text = "Download CSA Tracker"
+        Me.bDownloadCSATracker.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        DataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle7
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.DataGridView1.DefaultCellStyle = DataGridViewCellStyle8
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 41)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        DataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle9.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.RowHeadersDefaultCellStyle = DataGridViewCellStyle9
+        Me.DataGridView1.RowHeadersWidth = 15
+        Me.DataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        Me.DataGridView1.Size = New System.Drawing.Size(757, 345)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'My_BgWorker
+        '
+        Me.My_BgWorker.WorkerReportsProgress = True
+        Me.My_BgWorker.WorkerSupportsCancellation = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripProgressBar1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 364)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(757, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(133, 17)
+        Me.ToolStripStatusLabel1.Text = "Initializing Download....."
+        Me.ToolStripStatusLabel1.Visible = False
+        '
+        'ToolStripProgressBar1
+        '
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(150, 16)
+        Me.ToolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee
+        Me.ToolStripProgressBar1.Visible = False
+        '
+        'COM_CSA_Tracker_v1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(757, 386)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "COM_CSA_Tracker_v1"
+        Me.Text = "COM CSA Tracker"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents bDownloadCSATracker As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents My_BgWorker As System.ComponentModel.BackgroundWorker
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripProgressBar1 As System.Windows.Forms.ToolStripProgressBar
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+End Class
---
+++ SP Get Records/SP Get Records/COM CSA Tracker v1.vb
@@ -0,0 +1,202 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports Microsoft.SharePoint.Client
+Imports SP = Microsoft.SharePoint.Client
+Imports System.Threading
+
+
+Public Class COM_CSA_Tracker_v1
+
+    Dim table As DataTable = New DataTable()
+    Dim bCancelLoad As Boolean = False
+    Dim m_CountTo As Integer = 0 ' How many time to loop.
+    Dim iListCount As Integer
+
+
+    Private Sub COM_CSA_Tracker_v1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        Label1.Text = "Total Records: ---"
+
+        table.Columns.Add("Id")
+        table.Columns.Add("Title")
+        table.Columns.Add("Rep Name")
+        table.Columns.Add("Category")
+        table.Columns.Add("Request")
+        table.Columns.Add("Level of Difficulty")
+        table.Columns.Add("Details")
+        table.Columns.Add("Resolved")
+        table.Columns.Add("Team")
+        table.Columns.Add("Team Lead")
+        table.Columns.Add("field1")
+        table.Columns.Add("Date")
+        table.Columns.Add("Target Audiences ")
+        table.Columns.Add("SRC#")
+        table.Columns.Add("Order#")
+        table.Columns.Add("Created")
+        table.Columns.Add("Created By")
+        table.Columns.Add("Modified")
+        table.Columns.Add("Modified By")
+
+        DataGridView1.DataSource = table
+
+        DataGridView1.Columns(0).Width = 30
+        DataGridView1.Columns(1).Width = 80
+        DataGridView1.Columns(2).Width = 80
+        DataGridView1.Columns(3).Width = 80
+        DataGridView1.Columns(4).Width = 80
+        DataGridView1.Columns(5).Width = 50
+
+        DataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText
+
+
+    End Sub
+
+    Private Sub bDownloadCSATracker_Click(sender As System.Object, e As System.EventArgs) Handles bDownloadCSATracker.Click
+
+        If bDownloadCSATracker.Text = "Download CSA Tracker" Then
+            bDownloadCSATracker.Text = "Cancel Download"
+            bDownloadCSATracker.Refresh()
+
+            ToolStripProgressBar1.Style = ProgressBarStyle.Marquee
+            ToolStripProgressBar1.Visible = True
+
+            ToolStripStatusLabel1.Text = "Initializing Download....."
+            ToolStripStatusLabel1.Visible = True
+
+            Label1.Text = "Total Records: 0"
+            Label1.Visible = False
+
+
+            table.Clear()
+            DataGridView1.Refresh()
+
+            My_BgWorker.RunWorkerAsync()
+
+        ElseIf bDownloadCSATracker.Text = "Cancel Download" Then
+            My_BgWorker.CancelAsync()
+            ToolStripStatusLabel1.Text = "Cancelling Download....."
+            bDownloadCSATracker.Text = "Cancel Download."
+        End If
+
+    End Sub
+
+
+
+    Private Sub My_BgWorker_DoWork(ByVal sender As Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles My_BgWorker.DoWork
+
+        Dim i As Integer = 0
+        Dim siteUrl As String = "http://intranet.afservice.org/afs/com"
+
+        Dim clientContext As New ClientContext(siteUrl)
+        Dim oList As List = clientContext.Web.Lists.GetByTitle("CSAtracker")
+
+        Dim itemPosition As ListItemCollectionPosition = Nothing
+        Dim flvCreatedBy As SP.FieldUserValue
+        Dim sCreatedBy As String
+
+        Dim flvModifiedBy As SP.FieldUserValue
+        Dim sModifiedBy As String
+
+        table.Clear()
+
+
+        While Not My_BgWorker.CancellationPending
+
+
+            Dim camlQuery As New CamlQuery()
+            camlQuery.ListItemCollectionPosition = itemPosition
+            camlQuery.ViewXml = "<View Scope='RecursiveAll'><RowLimit>1903</RowLimit></View>"
+
+            'camlQuery.ViewXml = "<View Scope='Recursive'><Query><Where>" _
+            '    & "<Geq>" _
+            '    & "<FieldRef Name='ID'/><Value Type='Number'>0</Value>" _
+            '    & "</Geq>" _
+            '    & "</Where></Query><RowLimit>100</RowLimit></View>"
+
+
+            Dim collListItem As ListItemCollection = oList.GetItems(camlQuery)
+
+            Try
+                collListItem = oList.GetItems(camlQuery)
+                clientContext.Load(collListItem)
+                clientContext.ExecuteQuery()
+                itemPosition = collListItem.ListItemCollectionPosition
+                'If Not itemPosition Is Nothing Then Console.WriteLine(itemPosition.PagingInfo)
+                'Console.WriteLine(collListItem.Count.ToString())
+
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Information, "Access Error")
+                e.Cancel = True
+                Exit Sub
+            End Try
+
+
+            Dim oListItem As ListItem
+
+
+            For Each oListItem In collListItem
+
+                flvCreatedBy = oListItem("Author")
+                sCreatedBy = flvCreatedBy.LookupValue
+
+                flvModifiedBy = oListItem("Editor")
+                sModifiedBy = flvModifiedBy.LookupValue
+
+                table.Rows.Add(oListItem.Id, oListItem("Title"), oListItem.Item("Rep_x0020_Name"), oListItem("Type1"), oListItem("Request"), oListItem("Level_x0020_of_x0020_Difficulty"), oListItem("Details"), oListItem("Resolved"), oListItem("Team"), oListItem("Team_x0020_Lead"), oListItem("field1"), CDate(oListItem("Date")).ToString("d"), oListItem("Target_x0020_Audiences"), oListItem("SRC_x0023_"), oListItem("Order_x0023_"), oListItem("Created"), sCreatedBy, oListItem("Modified"), sModifiedBy)
+
+                i += 1
+
+            Next oListItem
+
+            'Thread.Sleep(500)
+            My_BgWorker.ReportProgress(i)
+
+            If itemPosition Is Nothing Then Exit While
+            If My_BgWorker.CancellationPending Then e.Cancel = True
+
+        End While
+
+
+
+
+    End Sub
+
+
+    Private Sub My_BgWorker_ProgressChanged(sender As System.Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles My_BgWorker.ProgressChanged
+
+        'If ToolStripProgressBar1.Style = ProgressBarStyle.Marquee Then
+        '    ToolStripProgressBar1.Style = ProgressBarStyle.Continuous
+        '    ToolStripProgressBar1.Maximum = iListCount + 2000
+        '    ToolStripProgressBar1.Step = 200
+        'End If
+
+        'ToolStripProgressBar1.Value = e.ProgressPercentage
+        ToolStripStatusLabel1.Text = "Downloading Records...    (" & e.ProgressPercentage.ToString("#,#;(#,#)") & " of ....)"
+
+        Label1.Text = "Total Records: " & e.ProgressPercentage.ToString("#,#;(#,#)")
+        Label1.Refresh()
+
+
+        DataGridView1.Refresh()
+
+    End Sub
+
+    Private Sub My_BgWorker_RunWorkerCompleted(sender As System.Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles My_BgWorker.RunWorkerCompleted
+
+        bDownloadCSATracker.Text = "Download CSA Tracker"
+        bDownloadCSATracker.Refresh()
+        ToolStripProgressBar1.Visible = False
+        ToolStripStatusLabel1.Visible = False
+        Label1.Visible = True
+
+        If Not e.Cancelled Then
+            MsgBox("Download completed!", MsgBoxStyle.Information, "Download Completed")
+        Else
+            MsgBox("Download cancelled!", MsgBoxStyle.Information, "Download Cancelled")
+        End If
+
+    End Sub
+
+
+
+End Class
